0e330f55a16425b9a2507fada6ba425a24884386
Stephen <stephen@umw.edu>
2022-03-21 00:15:10 -0400

    Good enough starter. Multiple pages interconnected, and takes input.

--- routes.py
+++ routes.py
@@ -18,4 +18,52 @@ def chooseflavor():
 
 @bj.route("/browserecipes")
 def browserecipes():
-    return f"<html><body>The flavor is: {request.args['flavor']}</body></html>"
+    if 'flavor' not in request.args:
+        return redirect(url_for("chooseflavor"))
+    conn = sqlite3.connect(os.path.join(current_app.root_path,"bj.sqlite"))
+    recipes = conn.cursor().execute("""
+        select name from recipe
+        where flavorName=?
+    """, (request.args['flavor'],)).fetchall()
+
+    return render_template("browserecipes.html", flavor=request.args['flavor'],
+        recipes=recipes)
+
+@bj.route("/viewrecipe", methods=['GET','POST'])
+def viewrecipe():
+    if request.method=='GET' and 'recipe' not in request.args:
+        return redirect(url_for("chooseflavor"))
+    elif 'numCartons' not in request.form:
+        conn = sqlite3.connect(os.path.join(current_app.root_path,"bj.sqlite"))
+        ingredients = conn.cursor().execute("""
+            select mixin_name, costPerOz
+            from mixin join ingredients on mixin.name=ingredients.mixin_name
+            where recipe_name=?
+        """, (request.args['recipe'],)).fetchall()
+        baseFlavor = conn.cursor().execute("""
+            select flavorName from recipe
+            where name=?
+        """, (request.args['recipe'],)).fetchone()
+        cartonsOrdered = conn.cursor().execute("""
+            select cartonsOrdered from recipe
+            where name=?
+        """, (request.args['recipe'],)).fetchone()
+        
+        return render_template("viewrecipe.html", recipe=request.args['recipe'],
+            ingredients=ingredients, baseFlavor=baseFlavor[0],
+            cartonsOrdered=cartonsOrdered[0])
+    else:
+        conn = sqlite3.connect(os.path.join(current_app.root_path,"bj.sqlite"))
+        conn.cursor().execute("""
+            update recipe set cartonsOrdered=cartonsOrdered+?
+            where name=?
+        """, (request.form['numCartons'],request.form['recipe']))
+        conn.commit()
+        newCartonsOrdered = conn.cursor().execute("""
+            select cartonsOrdered from recipe where name=?
+        """, (request.form['recipe'],)).fetchone()
+        return render_template("confirmorder.html",
+            recipe=request.form['recipe'],
+            newCartonsOrdered=newCartonsOrdered)
+
+    
